bc34ad97ad161502d952b23b98367c0df8c5fc9a
Joseph Weston <joseph.weston08@gmail.com>
2016-10-09 01:22:43 +0200

    fix email accounts

--- mutt/get_pass.py
+++ mutt/get_pass.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python2
 
 from subprocess import check_output
+import os
 
 def get_pass(account):
-    return check_output("gpg -dq ~/.mutt/passwd/%s.gpg" % account,
-                        shell=True).strip('\n')
+    return check_output("pass {}".format(account), shell=True, env=os.environ).strip('\n')
