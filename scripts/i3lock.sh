73fc584051de9526c83d3bae332fe102d6dbc244
Joseph Weston <joseph.weston08@gmail.com>
2017-05-15 14:19:00 +0200

    update screenlock to check us out of any projects before locking

--- scripts/i3lock.sh
+++ scripts/i3lock.sh
@@ -7,9 +7,16 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 composite -gravity center "$tmpbg" "$tmpbg"
 
-#enable i3lock with colours & modified image
-i3lock --textcolor=ffffff00 --insidecolor=ffffff00 --ringcolor=ffffff00 --linecolor=ffffff00 --keyhlcolor=FF000080 --ringvercolor=0000FF00 --insidevercolor=00000000 --ringwrongcolor=00000055 --insidewrongcolor=FF00001c  -i "$tmpbg"
+# clock out of project
+current_project="$(t cur)"
+[ "$current_project" ] && t out
 
-i3lock  -i "$tmpbg"
+#enable i3lock with colours & modified image
+i3lock  -i "$tmpbg" --nofork &
+wait
+
+# clock back in to project
+[ "$current_project" ] && t in
+
 #clean up
 rm "$tmpbg"
